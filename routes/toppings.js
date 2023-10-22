afac567599a765016eac6509f69f921514e00298
ailenrmansilla <ailen.r.mansilla@gmail.com>
2023-10-22 01:47:43 -0400

    toppings route corrected

--- routes/toppings.js
+++ routes/toppings.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const iceCreamShopController = require('../controllers/toppings');
 
-router.get('/toppings', iceCreamShopController.getAllToppings); // HTTP verb, URL path/pattern, function called to handle that pattern
+router.get('/', iceCreamShopController.getAllToppings); // HTTP verb, URL path/pattern, function called to handle that pattern
 router.get('/:id', iceCreamShopController.getSingleTopping); // /:id = path/pattern  (the req in the getSinlge function)
 router.post('/', iceCreamShopController.createTopping);
 router.put('/:id', iceCreamShopController.updateTopping);
