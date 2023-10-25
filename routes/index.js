0131dba08c9601f022717832f7bcc9e8bcc8f433
ailenrmansilla <ailen.r.mansilla@gmail.com>
2023-10-25 10:12:06 -0400

    correction

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const userDataValidate = require("../validation/validate");
 
 //routes
 router.use('/', require('./swagger'));
